5f3a7c1855c42b8b5542264a463e9b85f36db872
Matthew Kimball <mekimba6@gmail.com>
2022-01-18 11:15:29 -0700

    works on booksearchcontroller

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::BookSearchController < ApplicationController
     if params[:quantity].to_i > 1
       data, forecast, books = BookFacade.search_book(params[:location], params[:quantity])
     require 'pry'; binding.pry
+    else
+      render json: {error: 'Search quantity must be greater than zero'}
     end
   end
 end
--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,8 +1,15 @@
 class BookFacade
   def self.search_book(location, quantity = 1)
     books = BookService.book_search(location, quantity)
-    books[:docs].map do |book|
+    mapped_books = books[:docs].map do |book|
       Book.new(book)
     end
+    return mapped_books, location_weather(location)
+  end
+
+  def self.location_weather(location)
+    coords = MapquestFacade.get_location(location)
+    weather = WeatherService.get_weather(coords.lattitude, coords.longitude)
+    Weather.new(weather)
   end
 end
