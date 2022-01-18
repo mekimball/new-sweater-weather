c201b99c8e38e29bce3fae272055fbf00865aca5
Matthew Kimball <mekimba6@gmail.com>
2022-01-18 10:06:25 -0700

    adds book poros and spec

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,5 +1,5 @@
 class BookFacade
-  def self.search_book(location, limit)
+  def self.search_book(location, limit = 1)
     book = BookService.book_search(location, limit)
     Book.new(book)
   end
---
+++ spec/poros/book_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Book do
+  before do
+    @book = BookFacade.search_book('Denver,co')
+  end
+
+  it 'exists', :vcr do
+    expect(@book).to be_a(Book)
+  end
+
+  it 'has attributes', :vcr do
+    expect(@book.title).to eq('Denver, Co')
+    expect(@book.author).to eq('Universal Map')
+    expect(@book.isbn).to eq(["9780762507849", "0762507845"])
+    expect(@book.publisher).to eq(["Universal Map Enterprises"])
+  end
+end
